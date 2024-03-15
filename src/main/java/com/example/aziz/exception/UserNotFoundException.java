25e7b8f233815d68355f3775193bb83557b9dcc6
brahgithubsal <brahsallemi99@gmail.com>
2024-03-15 13:26:46 +0100

    Adding Spring Boot application files

---
+++ src/main/java/com/example/aziz/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.aziz.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
